996145858137f3db53c571b6bb54f1c37a2b3fb0
siltreza-code <siltreza@tutamail.com>
2025-12-10 19:55:13 -0800

    Add default theme configuration file

---
+++ Themes/Default.lua
@@ -0,0 +1,28 @@
+return {
+	MainColor = Color3.fromRGB(60, 60, 60),
+	AccentColor = Color3.fromRGB(255, 165, 192),
+	BackgroundColor = Color3.fromRGB(40, 40, 40),
+	FontColor = Color3.fromRGB(220, 220, 220),
+	FontSecondary = Color3.fromRGB(180, 180, 180),
+	FontDisabled = Color3.fromRGB(120, 120, 120),
+	OutlineColor = Color3.fromRGB(10, 10, 10),
+	BackgroundImage = 12807408896,
+	Buttons = {
+		Normal = Color3.fromRGB(99, 70, 80),
+		Hover = Color3.fromRGB(158, 112, 128),
+		Click = Color3.fromRGB(206, 146, 167),
+		Disabled = Color3.fromRGB(59, 42, 48)
+	},
+	Toggles = {
+		On = Color3.fromRGB(121, 255, 132),
+		Off = Color3.fromRGB(255, 130, 130),
+		Disabled = Color3.fromRGB(59, 42, 48)
+	},
+	Sliders = {
+		SliderColor = Color3.fromRGB(90, 71, 79),
+		BoxColor = Color3.fromRGB(165, 121, 135),
+		Hover = Color3.fromRGB(182, 133, 150),
+	},
+	
+	-- Might add soon
+}
